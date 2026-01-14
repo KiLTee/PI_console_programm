fda33a6fd591ffea8ab0b615f35de8c10d3c4810
mip <example@example.com>
2026-01-15 00:21:17 +1000

    Добвили test file

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 # 2
 # 3
 
+import test
+
 def main():
     print(input("Введите фразу для ее повторения: ")) # Код для дублирования ввода
+    print(test(1, 2))
 
 if __name__ == '__main__':
     main()
